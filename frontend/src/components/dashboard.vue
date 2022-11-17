ced31da460ede7d1a82377d8ebd5577e4f840f02
JoshuaASoto <joshua.antonio.soto@gmail.com>
2022-11-17 16:32:45 -0600

    Working Graph

--- frontend/src/components/dashboard.vue
+++ frontend/src/components/dashboard.vue
@@ -3,7 +3,6 @@
     <div>
       <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">Welcome to the Main Page</h1>
     </div>
-    <PlanetChart />
     <div>
       <EventChart
         v-if="!loading && !error"
@@ -34,51 +33,59 @@
 <script>
 import axios from "axios";
 import EventChart from "@/components/BarChart.vue";
-import PlanetChart from "@/components/PlanetChart.vue";
+
 export default {
   components: {
     EventChart,
-    PlanetChart,
   },
-    data() {
-        return {
-            databasedata: {},
-            labels:[],
-            attendees:[],
-            loading: false,
-            error: null,
-        };
-    },
+  data() {
+    return {
+      databasedata: {},
+      labels:[],
+      attendees:[],
+      loading: false,
+      error: null,
+    };
+  },
     methods: {
-      async fetchdata() {
-        try {
-          this.error = null;
-          this.loading = true;
-          let newnewapiURL = import.meta.env.VITE_ROOT_API + `/eventData/countall`;
-          axios.get(newnewapiURL).then((resp) => {;
-          this.databasedata = resp.data;
-          this.labels = resp.data.map((item) => item.eventName);
-          this.attendees = resp.data.map((item) => item.mycount);
-        })} catch (err) {
-          if (err.response) {
-            this.error = {
-              title: 'Server Response',
-              message: err.message,
-            };
-          } else if (err.request) {
-            this.error = {
-              title: "Unable to Reach Server",
-              message: err.message,
-            };
-          }
+    async fetchData() {
+      try {
+        this.error = null;
+        this.loading = true;
+        const url = `http://localhost:3000/eventData/countall`;
+        const resp = await axios.get(url);
+        //"re-organizing" - mapping json from the response
+        this.databasedata = resp.data;
+        this.labels = resp.data.map((item) => item.eventName);
+        this.attendees = resp.data.map((item) => item.mycount);
+      } catch (err) {
+        if (err.response) {
+          // client received an error response (5xx, 4xx)
+          this.error = {
+            title: "Server Response",
+            message: err.message,
+          };
+        } else if (err.request) {
+          // client never received a response, or request never left
+          this.error = {
+            title: "Unable to Reach Server",
+            message: err.message,
+          };
+        } else {
+          // There's probably an error in your code
+          this.error = {
+            title: "Application Error",
+            message: err.message,
+          };
         }
-        this.loading = false;
-      },
+      }
+      this.loading = false;
     },
-    mounted(){
-      this.fetchdata();
-    },
-  };
+  },
+  mounted() {
+    this.fetchData();
+  },
+};
 </script>
     // async mounted() {
     //     let newnewapiURL = import.meta.env.VITE_ROOT_API + `/eventData/countall`;
